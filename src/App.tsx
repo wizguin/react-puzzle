43fbd4fc2389c7007e415324f2cb8fcaebe801d8
wizguin <16276645+wizguin@users.noreply.github.com>
2025-05-25 14:01:11 +0100

    Add Puzzle to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 
+import Puzzle from './components/puzzle/Puzzle'
+
 export default function App() {
 
     return (
         <div id='container'>
             <h1>react-puzzle</h1>
+
+            <Puzzle />
         </div>
     )
 
