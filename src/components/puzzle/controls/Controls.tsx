2a091baca18ea5181cf8f47c163e31f5984246f4
wizguin <16276645+wizguin@users.noreply.github.com>
2025-05-26 21:23:41 +0100

    Add submitting turn

--- src/components/puzzle/controls/Controls.tsx
+++ src/components/puzzle/controls/Controls.tsx
@@ -1,8 +1,17 @@
 import './Controls.css'
 
+import { gridAtom, turnAtom, turnGridAtom } from '../../../atoms'
 import ControlButton from './control_button/ControlButton'
+import { resetTurnGrid } from '../../../utils/gridUtils'
+
+import { useAtom, useSetAtom } from 'jotai'
 
 export default function Controls() {
+
+    const setGrid = useSetAtom(gridAtom)
+    const [turnGrid, setTurnGrid] = useAtom(turnGridAtom)
+
+    const [turn, setTurn] = useAtom(turnAtom)
 
     const buttons = Array.from(Array(9), (_, index) =>
         <ControlButton
@@ -11,13 +20,34 @@ export default function Controls() {
         />
     )
 
+    function onSubmit() {
+        if (turn >= 9) {
+            return
+        }
+
+        // Merge values from turnGrid into grid
+        setGrid(prev => prev.map((box, boxIndex) =>
+            box.map((value, cellIndex) =>
+                turnGrid[boxIndex][cellIndex] || value
+            )
+        ))
+
+        setTurnGrid(resetTurnGrid())
+        setTurn(prev => prev + 1)
+    }
+
     return (
         <div id='controls'>
             <div id='controls__numbers'>
                 {buttons}
             </div>
 
-            <div id='controls__submit'>Submit Turn</div>
+            <div
+                id='controls__submit'
+                onClick={onSubmit}
+            >
+                Submit Turn
+            </div>
         </div>
     )
 
