9bd5c0d841e7ed770e748eb9af89ada23871a915
wizguin <16276645+wizguin@users.noreply.github.com>
2025-05-26 18:32:22 +0100

    Add control btns updating selected cell value

--- src/components/puzzle/controls/control_button/ControlButton.tsx
+++ src/components/puzzle/controls/control_button/ControlButton.tsx
@@ -1,13 +1,37 @@
 import './ControlButton.css'
 
+import { gridAtom, selectedCellAtom } from '../../../../atoms'
+
+import { useAtomValue, useSetAtom } from 'jotai'
+
 interface Props {
     value: number
 }
 
 export default function ControlButton({ value }: Props) {
 
+    const setGrid = useSetAtom(gridAtom)
+    const selectedCell = useAtomValue(selectedCellAtom)
+
+    function onClick() {
+        if (!selectedCell) {
+            return
+        }
+
+        setGrid(grid => {
+            const next = [...grid]
+
+            next[selectedCell.box][selectedCell.cell] = value
+
+            return next
+        })
+    }
+
     return (
-        <div className='control-button'>
+        <div
+            className='control-button'
+            onClick={onClick}
+        >
             {value}
         </div>
     )
