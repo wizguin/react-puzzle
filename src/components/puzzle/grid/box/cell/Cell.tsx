01bbd2cacae77cb4128d23fdca63a86d477f1a46
wizguin <16276645+wizguin@users.noreply.github.com>
2025-05-26 17:55:11 +0100

    Add selecting cells

--- src/components/puzzle/grid/box/cell/Cell.tsx
+++ src/components/puzzle/grid/box/cell/Cell.tsx
@@ -1,5 +1,10 @@
 import './Cell.css'
 
+import { classNames } from '../../../../../utils/cssUtils'
+import { selectedCellAtom } from '../../../../../atoms'
+
+import { useAtom } from 'jotai'
+
 interface Props {
     value: number,
     boxIndex: number,
@@ -8,8 +13,24 @@ interface Props {
 
 export default function Cell({ value, boxIndex, cellIndex }: Props) {
 
+    const [selectedCell, setSelectedCell] = useAtom(selectedCellAtom)
+
+    const active =
+        selectedCell?.box === boxIndex &&
+        selectedCell?.cell === cellIndex
+
+    function onClick() {
+        setSelectedCell({
+            box: boxIndex,
+            cell: cellIndex
+        })
+    }
+
     return (
-        <div className='cell'>
+        <div
+            className={classNames('cell', active && 'active')}
+            onClick={onClick}
+        >
             {value || ''}
         </div>
     )
