12110d7bbab5b3bccd4a103bc34347b332b0d021
wizguin <16276645+wizguin@users.noreply.github.com>
2025-05-28 11:08:20 +0100

    Add new game button

--- src/components/puzzle/game_over/GameOver.tsx
+++ src/components/puzzle/game_over/GameOver.tsx
@@ -1,6 +1,10 @@
 import './GameOver.css'
 
+import { generateSolution } from '../../../utils/gridUtils'
+import { solutionAtom } from '../../../atoms'
 import { Status } from '../Puzzle'
+
+import { useSetAtom } from 'jotai'
 
 interface Props {
     status: Status
@@ -8,13 +12,26 @@ interface Props {
 
 export default function GameOver({ status }: Props) {
 
+    const setSolution = useSetAtom(solutionAtom)
+
     const message = status === Status.Lost
-        ? 'You lost'
+        ? 'Game over!'
         : 'You won!'
+
+    function onNewClick() {
+        setSolution(generateSolution())
+    }
 
     return (
         <div id='game-over'>
             {message}
+
+            <button
+                className='btn-success'
+                onClick={onNewClick}
+            >
+                Start new game
+            </button>
         </div>
     )
 
